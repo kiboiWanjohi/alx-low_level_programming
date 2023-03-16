211595c654b624baa52d9eb38433c632bc263f87
kiboiWanjohi <sammy313.sw@gmail.com>
2023-03-16 13:00:32 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,6 @@
 /**
  * print_dlistint - print all the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
- *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
